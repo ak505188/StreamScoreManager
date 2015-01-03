151f6658d09fd37886f009e2c33ea91e83b8e812
ak505188 <alexandr.kolesnik.51@my.csun.edu>
2015-01-02 18:03:36 -0800

    Some basic file i/o done.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Hello World!");
+    public static void main(String[] args){
+        char[] Player1Name = new char[50];
+        try{
+            File Name1 = new File("P1_score.txt");
+            if (!Name1.exists())
+                Name1.createNewFile();
+            FileReader namer1 = new FileReader(Name1);
+            FileWriter namew1 = new FileWriter(Name1);
+            namew1.write("UG | Alex 80k");
+            namew1.flush();
+            namew1.close();
+            namer1.read(Player1Name);
+            for (int i = 0; i < 50; i++)
+                System.out.print(Player1Name[i]);
+            System.out.println();
+            namer1.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+        System.out.println("Hello World!");
     }
 }
