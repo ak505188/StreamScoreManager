971988d75b73a7ae1235d33204d8df670ce981a2
ak505188 <alexandr.kolesnik.51@my.csun.edu>
2015-01-09 16:40:49 -0800

    Working version. Creates/opens 4 text files in the same directory as program and edits them based off of input in the GUI.

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,124 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
+import java.awt.FlowLayout;
 
-public class Main {
+public class Main extends JFrame{
+    private static int Score1 = 0;
+    private static int Score2 = 0;
+    private static String Name1 = "";
+    private static String Name2 = "";
 
-    public static void main(String[] args){
-        char[] Player1Name = new char[50];
-        try{
-            File Name1 = new File("P1_score.txt");
-            if (!Name1.exists())
-                Name1.createNewFile();
-            FileReader namer1 = new FileReader(Name1);
-            FileWriter namew1 = new FileWriter(Name1);
-            namew1.write("UG | Alex 80k");
-            namew1.flush();
-            namew1.close();
-            namer1.read(Player1Name);
-            for (int i = 0; i < 50; i++)
-                System.out.print(Player1Name[i]);
-            System.out.println();
-            namer1.close();
-        }catch (IOException e){
+    static public void UpdateFiles (){
+        try {
+            FileWriter writer;
+            writer = new FileWriter("Name1.txt");
+            writer.flush();
+            writer.write(Name1);
+            writer.close();
+            writer = new FileWriter("Score1.txt");
+            writer.flush();
+            writer.write(String.valueOf(Score1));
+            writer.close();
+            writer = new FileWriter("Name2.txt");
+            writer.flush();
+            writer.write(Name2);
+            writer.close();
+            writer = new FileWriter("Score2.txt");
+            writer.flush();
+            writer.write(String.valueOf(Score2));
+            writer.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    } //UpdateFiles
+    static public void UpdateNames (String name1, String name2){
+        Name1 = name1;
+        Name2 = name2;
+    }//UpdateNames
+    static public int changeScore1(int num){
+        if (num < 0)
+            Score1++;
+        else Score1 = num;
+        return Score1;
+    }//changeScore1
+    static public int changeScore2(int num){
+        if (num < 0)
+            Score2++;
+        else Score2 = num;
+        return Score2;
+    }//changeScore2
 
-
-
-
-
-
-
-
-
-
-        System.out.println("Hello World!");
+    public Main() {
+        super("StreamScoreManager");
+        setLayout(new FlowLayout());
+        setSize(560, 150);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
-}
+
+    public static void main(String[] args){
+        Main frame = new Main();
+        JButton Plus1 = new JButton("+");
+        final JTextField TextScore1 = new JTextField(3);
+        final JTextField TextName1 = new JTextField(40);
+        JButton Plus2 = new JButton("+");
+        final JTextField TextScore2 = new JTextField(3);
+        final JTextField TextName2 = new JTextField(40);
+        JButton Apply = new JButton("Apply");
+        JButton ResetScores = new JButton("Reset Scores");
+        JButton ClearAll = new JButton("Clear All");
+        frame.add(Plus1);
+        frame.add(TextScore1);
+        frame.add(TextName1);
+        frame.add(Plus2);
+        frame.add(TextScore2);
+        frame.add(TextName2);
+        frame.add(Apply);
+        frame.add(ResetScores);
+        frame.add(ClearAll);
+        TextScore1.setText("0");
+        TextScore2.setText("0");
+        Plus1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TextScore1.setText(String.valueOf(changeScore1(-1)));
+                UpdateFiles();
+            }
+        });
+        Plus2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TextScore2.setText(String.valueOf(changeScore2(-1)));
+                UpdateFiles();
+            }
+        });
+        Apply.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UpdateNames(TextName1.getText(), TextName2.getText());
+                UpdateFiles();
+            }
+        });
+        ResetScores.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TextScore1.setText(String.valueOf(changeScore1(0)));
+                TextScore2.setText(String.valueOf(changeScore2(0)));
+                UpdateFiles();
+            }
+        });
+        ClearAll.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TextScore1.setText(String.valueOf(changeScore1(0)));
+                TextScore2.setText(String.valueOf(changeScore2(0)));
+                TextName1.setText("");
+                TextName2.setText("");
+                UpdateNames("", "");
+                UpdateFiles();
+            }
+        });
+        frame.setVisible(true);
+    }//main
+}//Main
